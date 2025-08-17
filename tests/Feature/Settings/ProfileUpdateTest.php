2b8b45c255a516c3bbc292261e7705fdae597e71
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-08-17 19:40:05 +0800

    Removed unnecessary tests

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -50,23 +50,6 @@ test('email verification status is unchanged when the email address is unchanged
     expect($user->refresh()->email_verified_at)->not->toBeNull();
 });
 
-test('user can delete their account', function () {
-    $user = User::factory()->create();
-
-    $response = $this
-        ->actingAs($user)
-        ->delete('/settings/profile', [
-            'password' => 'password',
-        ]);
-
-    $response
-        ->assertSessionHasNoErrors()
-        ->assertRedirect('/');
-
-    $this->assertGuest();
-    expect($user->fresh())->toBeNull();
-});
-
 test('correct password must be provided to delete account', function () {
     $user = User::factory()->create();
 
