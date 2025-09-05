f0e74e468b666377c6dd78a746240e4bf7f31daf
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-09-05 13:01:43 +0800

    Update https config

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -1,50 +1,50 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-declare global {
-    interface Window {
-        Pusher: typeof Pusher;
-        Echo: any;
-    }
-}
+// declare global {
+//     interface Window {
+//         Pusher: typeof Pusher;
+//         Echo: any;
+//     }
+// }
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-    authorizer: (channel: any) => {
-        return {
-            authorize: (socketId: string, callback: Function) => {
-                fetch('/broadcasting/auth', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || '',
-                        'X-Requested-With': 'XMLHttpRequest',
-                    },
-                    credentials: 'same-origin',
-                    body: JSON.stringify({
-                        socket_id: socketId,
-                        channel_name: channel.name,
-                    }),
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        callback(null, data);
-                    })
-                    .catch((error) => {
-                        callback(error, null);
-                    });
-            },
-        };
-    },
-});
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
+//     authorizer: (channel: any) => {
+//         return {
+//             authorize: (socketId: string, callback: Function) => {
+//                 fetch('/broadcasting/auth', {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                         Accept: 'application/json',
+//                         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || '',
+//                         'X-Requested-With': 'XMLHttpRequest',
+//                     },
+//                     credentials: 'same-origin',
+//                     body: JSON.stringify({
+//                         socket_id: socketId,
+//                         channel_name: channel.name,
+//                     }),
+//                 })
+//                     .then((response) => response.json())
+//                     .then((data) => {
+//                         callback(null, data);
+//                     })
+//                     .catch((error) => {
+//                         callback(error, null);
+//                     });
+//             },
+//         };
+//     },
+// });
 
-export default window.Echo;
+// export default window.Echo;
